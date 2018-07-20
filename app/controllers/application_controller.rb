c3480dac2b2d9984dde897ed7313d54839a62b4b
Christine Cua <cloudycua@gmail.com>
2018-07-20 18:24:00 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,12 +5,6 @@ class App < Sinatra::Base
     set :views, Proc.new { File.join(root, "../views/") }
 
     get '/' do
-      erb :super_hero
-    end
-
-    post '/teams' do
-      @team =
-
-      erb :team
+      erb :index
     end
 end
