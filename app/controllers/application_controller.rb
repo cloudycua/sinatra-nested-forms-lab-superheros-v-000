073d443dc0d73568313292a2c51d89eafc319ca6
Christine Cua <cloudycua@gmail.com>
2018-07-20 18:57:55 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,10 +10,10 @@ class App < Sinatra::Base
 
     post '/teams' do
       @team = Team.new(params[:team])
-      binding.pry
       params[:team][:heroes].each do |details|
         Hero.new(details)
       end
+      binding.pry
       @heroes = Hero.all
     end
 
